8034798d257856288cfeab9b5436366942e50bd7
arsyad12 <33723389+arsyad12@users.noreply.github.com>
2023-10-05 20:54:30 +0700

    Make endpoint users/me
    make endpoint for get detail user with JWT token

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const database = require("./database");
 const { request } = require("http");
 let port = process.env.PORT;
 const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken');
 
 
 
@@ -12,6 +13,25 @@ const bcrypt = require('bcrypt');
 app.use(express.urlencoded({ extended: false }));
 //for tell the express the data is json
 app.use(express.json());
+
+//middle ware for GET DETAIL USER by token
+const checkJWT = async (req,res,next) =>{
+  try {
+    const token = req.headers.authorization.slice(7) // get token from authoriztion and slice 7 string in the front jwt
+    const decoded = jwt.verify(token, process.env.APP_SECRET_TOKEN)
+    console.log("decoded", decoded)
+    
+    if (decoded) { // if decode succes next to execution function
+      next()
+    }
+    
+  } catch (error) {
+    res.status(401).json({
+      status : false,
+      massage : "token error",
+    })
+  }
+}
 
 //in app.get we can use for write the rest API
 // endpoint movies is for called all data wee needed from table movies
@@ -368,7 +388,7 @@ app.delete("/cinemas/:id", async (req, res) => {
 // end point /users -> for get data user except password and email
 // end point /users/me -> for get all data with password and email
 // end point users/register -> for post data user, it must with password encryption
-// end point users/login -> for acces data with token JWT(json web token)
+// end point users/login -> for acces detail data with token JWT(json web token)
 // end point users/edit -> for edit data with encrypton after login with JWT
 
 //get all user
@@ -477,7 +497,8 @@ app.post("/users", async (req, res) => {
 
 })
 
-// update user
+// LOGIN User, check by email and passwor
+//make Token with JWT 
 
 app.post("/users/login", async (req, res) => {
 try {
@@ -487,6 +508,7 @@ try {
   //check if email registered
   const checkEmail = await database `SELECT * FROM users WHERE email =${email}`
 
+  // validation if email not registered
   if (checkEmail.length == 0) {
     res.status(404).json({
       status : "false",
@@ -495,14 +517,15 @@ try {
     return;
   }
 
-
    //check if password correct
   const isMatch = bcrypt.compareSync(password, checkEmail[0].password);
 
   if (isMatch) {
+    const token = jwt.sign(checkEmail[0], process.env.APP_SECRET_TOKEN); //make token and return data credential by email
     res.status(200).json({
       status : "True",
       massage : "Login succes",
+      accesToken : token
     })
   }else{
     res.status(400).json({
@@ -510,6 +533,7 @@ try {
       massage : "Password incorrect",
   })
 }
+
 
 
 } catch (error) {
@@ -521,9 +545,28 @@ try {
 })
 
 
-// delete user
+// GET Detail User
 
-app.post("/cinemas", async (req, res) => {
+app.get("/users/me", checkJWT, async (req, res) => {
+
+  try {
+
+    const token = req.headers.authorization.slice(7) // get token from authoriztion and slice 7 string in the front jwt
+    const decoded = jwt.verify(token, process.env.APP_SECRET_TOKEN) //verify the token with env jwt
+    const request = await database `SELECT * FROM users where id = ${decoded.id}` //get data bye token id
+
+    res.json({
+      status : "true",
+      massage :"get data succes",
+      data : request
+    })
+  } catch (error) {
+    res.json({
+      status : false,
+      massage : "something wron in server",
+      data : []
+  })
+  }
 
 })
 
